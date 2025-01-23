12a79144b0e690d6f1ce8628b6b27e3d406d54b1
levi boardman <leviboardman456@gmail.com>
2025-01-23 13:30:49 -0600

    commit

--- quiz2/Program.cs
+++ quiz2/Program.cs
@@ -10,6 +10,25 @@ namespace quiz2
     {
         static void Main(string[] args)
         {
+            //ask for item price
+            Console.WriteLine("Enter item price");
+            //wait for user input
+            double price = Convert.ToDouble(Console.ReadLine());
+            //ask for sales tax
+            Console.WriteLine("Enter sales tax (enter decimal number for percentage such as 0.05 for 5%)");
+            //wait for user input
+            double tax = Convert.ToDouble(Console.ReadLine());
+            //ask for promotion discount
+            Console.WriteLine("Enter promotion discount (enter decimal number for percentage such as 0.05 for 5%)");
+            //wait for user input
+            double discount = Convert.ToDouble(Console.ReadLine());
+            //calculate total
+            double total = price * tax / discount;
+            //disp total
+            Console.WriteLine("Total cost: " + total);
+            //pause for user input
+            Console.ReadLine();
+
         }
     }
 }
